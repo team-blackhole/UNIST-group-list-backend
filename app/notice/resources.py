06ed74ebb9d97dd0ac023b85d053c33cdc24398f
Pelly Ryu <pelly.ryu@gmail.com>
2018-02-08 17:22:27 +0900

    Fix wrong response of NoticeDetail #4

--- app/notice/resources.py
+++ app/notice/resources.py
@@ -1,11 +1,7 @@
-from app import db
+from flask import Blueprint
+from flask.ext.restful import abort, reqparse
+from flask_restful import Api, Resource
 
-from flask import Blueprint, g
-
-from flask_restful import Api, Resource 
-from flask.ext.restful import abort, fields, marshal_with, reqparse
-
-from app.base.decorators import login_required, has_permissions
 from app.notice.models import Notice
 
 notice_bp = Blueprint('notice_api', __name__)
@@ -13,17 +9,15 @@ api = Api(notice_bp)
 
 
 class NoticeDetail(Resource):
-
     def get(self, notice_id):
         notice = Notice.query.filter_by(id=notice_id)
-        if not notice:
+        if not notice or notice.count() == 0:
             abort(404, message="Notice {} doesn't exist".format(notice_id))
         serialized_list = list(map(lambda x: x.serialize(), notice))
         return serialized_list
 
 
 class NoticeList(Resource):
-
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('page', type=int)
@@ -41,4 +35,3 @@ class NoticeList(Resource):
 
 api.add_resource(NoticeDetail, '/<notice_id>')
 api.add_resource(NoticeList, '/list')
-
